1a81c0b8d196e7f4b8d98df9b647c1519831e28f
Marcus B. Akre <marcus.akre@gmail.com>
2015-11-11 19:10:27 +0000

    Menu fix + test test

--- tests/spec/multichoice.spec.js
+++ tests/spec/multichoice.spec.js
@@ -16,8 +16,8 @@ stream = require('stream');
     .build();
  */
  beforeEach(function() {
-     driver.manage().timeouts().setScriptTimeout(6000);
-     driver.manage().window().setSize(400, 800);
+     driver.manage().timeouts().setScriptTimeout(60*1000);
+     driver.manage().window().setSize(1920, 1280);
 
  });
 
@@ -29,35 +29,56 @@ stream = require('stream');
 describe('basic test', function () {
 	it('should be on correct page', function (done) {
 
-        driver.get('http://wingify.com');
-
-        driver.takeScreenshot().then(function (png) {
-            var stream = fs.createWriteStream('/var/lib/jenkins/jobs/Multichoice/workspace/xblock_development/multichoice/tests/images/asdf.png');
-            stream.write(new Buffer(png, 'base64'));
-            stream.end();
-
-        });
-
-        var start = new Date().getTime();
-        driver.executeAsyncScript(
-                'window.setTimeout(arguments[arguments.length - 1], 500);').
-            then(function() {
-                console.log(
-                    'Elapsed time: ' + (new Date().getTime() - start) + ' ms');
-            });
-
-        driver.takeScreenshot().then(function (png) {
-            var stream = fs.createWriteStream('/var/lib/jenkins/jobs/Multichoice/workspace/xblock_development/multichoice/tests/images/asdf2.png');
-            stream.write(new Buffer(png, 'base64'));
-            stream.end();
-
-        });
+        driver.get('http://edx.akre.biz:8000/');
 
         driver.getTitle().then(function(title) {
-			expect(title).toBe('Wingify');
-			// Jasmine waits for the done callback to be called before proceeding to next specification.
-			done();
-		});
+            expect(title).toBe('XBlock scenarios');
+            done();
+        });
+
+        driver.takeScreenshot().then(function (png) {
+            var stream = fs.createWriteStream('/var/lib/jenkins/jobs/Multichoice/workspace/xblock_development/multichoice/tests/images/01-xblock-menu.png');
+            stream.write(new Buffer(png, 'base64'));
+            stream.end();
+
+        });
+
+        driver.get('http://edx.akre.biz:8000/scenario/multichoice.0/student_view');
+
+        driver.getTitle().then(function(title) {
+            expect(title).toBe('XBlock: MultiChoiceXBlock');
+            done();
+        });
+
+        driver.takeScreenshot().then(function (png) {
+            var stream = fs.createWriteStream('/var/lib/jenkins/jobs/Multichoice/workspace/xblock_development/multichoice/tests/images/02-student-view.png');
+            stream.write(new Buffer(png, 'base64'));
+            stream.end();
+        });
+
+
+
+/*
+        driver.get('http://edx.akre.biz:8000/scenario/multichoice.0/student_view');
+        driver.takeScreenshot().then(function (png) {
+            var stream = fs.createWriteStream('/var/lib/jenkins/jobs/Multichoice/workspace/xblock_development/multichoice/tests/images/02-student-view.png');
+            stream.write(new Buffer(png, 'base64'));
+            stream.end();
+
+        });
+*/
+
+        /*
+                var start = new Date().getTime();
+                driver.executeAsyncScript(
+                        'window.setTimeout(arguments[arguments.length - 1], 500);').
+                    then(function() {
+                        console.log(
+                            'Elapsed time: ' + (new Date().getTime() - start) + ' ms');
+                    });
+        */
+
+
 
 	}, 10000);
 });
